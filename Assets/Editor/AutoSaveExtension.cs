8e9739cd7832ef635ce6c84a071c81bc748ef15a
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-07-01 20:48:54 +0700

    feat: better auto save script

---
+++ Assets/Editor/AutoSaveExtension.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Editor
+{
+    [InitializeOnLoad]
+    public static class AutoSaveExtension
+    {
+        private const float WAIT_TIME_IN_SECONDS = 300;
+        private static float timer;
+
+        static AutoSaveExtension()
+        {
+            EditorApplication.playModeStateChanged += AutoSaveWhenPlaymodeStarts;
+            EditorApplication.update += AutoSaveEveryXSeconds;
+        }
+
+        private static void AutoSaveWhenPlaymodeStarts(PlayModeStateChange playModeStateChange)
+        {
+            if (playModeStateChange == PlayModeStateChange.ExitingEditMode) Save();
+        }
+
+        private static void AutoSaveEveryXSeconds()
+        {
+            if (Time.realtimeSinceStartup - timer > WAIT_TIME_IN_SECONDS && !EditorApplication.isPlaying &&
+                SceneManager.GetActiveScene().isDirty)
+                Save();
+        }
+
+        private static void Save()
+        {
+            EditorSceneManager.SaveOpenScenes();
+            AssetDatabase.SaveAssets();
+            timer = Time.realtimeSinceStartup;
+            Debug.Log("Auto saved.");
+        }
+    }
+}
