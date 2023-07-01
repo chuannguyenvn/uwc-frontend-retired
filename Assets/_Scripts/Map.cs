b05314f329037bc33c79a08288095ffb4d1fd80b
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-07-01 15:53:10 +0700

    feat: map script

---
+++ Assets/_Scripts/Map.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System;
+
+public class Map : MonoBehaviour
+{
+    public string apiKey;
+    public float lat = -33.85660618894087f;
+    public float lon = 151.21500701957325f;
+    public int zoom = 12;
+    public enum resolution { low = 1, high = 2 };
+    public resolution mapResolution = resolution.low;
+    public enum type { roadmap, satellite, gybrid, terrain };
+    public type mapType = type.roadmap;
+    private string url = "";
+    private int mapWidth = 640;
+    private int mapHeight = 640;
+    private bool mapIsLoading = false; //not used. Can be used to know that the map is loading 
+    private Rect rect;
+
+    private string apiKeyLast;
+    private float latLast = -33.85660618894087f;
+    private float lonLast = 151.21500701957325f;
+    private int zoomLast = 12;
+    private resolution mapResolutionLast = resolution.low;
+    private type mapTypeLast = type.roadmap;
+    private bool updateMap = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetGoogleMap());
+        rect = gameObject.GetComponent<RawImage>().rectTransform.rect;
+        mapWidth = (int)Math.Round(rect.width);
+        mapHeight = (int)Math.Round(rect.height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (updateMap && (apiKeyLast != apiKey || !Mathf.Approximately(latLast, lat) || !Mathf.Approximately(lonLast, lon) || zoomLast != zoom || mapResolutionLast != mapResolution || mapTypeLast != mapType))
+        {
+            rect = gameObject.GetComponent<RawImage>().rectTransform.rect;
+            mapWidth = (int)Math.Round(rect.width);
+            mapHeight = (int)Math.Round(rect.height);
+            StartCoroutine(GetGoogleMap());
+            updateMap = false;
+        }
+    }
+
+
+    IEnumerator GetGoogleMap()
+    {
+        url = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=" + zoom + "&size=" + mapWidth + "x" + mapHeight + "&scale=" + 5 + "&maptype=" + mapType + "&key=" + apiKey;
+        mapIsLoading = true;
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        yield return www.SendWebRequest();
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("WWW ERROR: " + www.error);
+        }
+        else
+        {
+            mapIsLoading = false;
+            gameObject.GetComponent<RawImage>().texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+
+            apiKeyLast = apiKey;
+            latLast = lat;
+            lonLast = lon;
+            zoomLast = zoom;
+            mapResolutionLast = mapResolution;
+            mapTypeLast = mapType;
+            updateMap = true;
+        }
+    }
+
+}
